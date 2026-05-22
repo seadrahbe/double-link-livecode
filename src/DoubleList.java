97a417fb1a3d81391ba2d2f1e39abe150c5f675d
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-05-21 17:07:56 -0700

    Finished livecode

--- src/DoubleList.java
+++ src/DoubleList.java
@@ -1,10 +1,71 @@
 public class DoubleList {
-  public DoubleList() {
 
+  private class Node {
+    public int data;
+    public Node next;
+    public Node prev;
+
+    public Node(int data) {
+      this.data = data;
+    }
+  }
+
+  private Node head;
+  private Node tail;
+
+  public DoubleList() {
+    head = null;
+    tail = null;
+
+  }
+
+  // add new data to end of list
+  public void append(int data) {
+    Node newNode = new Node(data);
+
+    if (head == null) {
+      head = newNode;
+    } else {
+      tail.next = newNode;
+      newNode.prev = tail;
+    }
+
+    tail = newNode;
+  }
+
+  public int getFirst() {
+    if (head == null) {
+      throw new IndexOutOfBoundsException("Cannot get first data of empty list");
+    }
+
+    return head.data;
+  }
+
+    public int getLast() {
+    if (head == null) {
+      throw new IndexOutOfBoundsException("Cannot get first data of empty list");
+    }
+
+    return tail.data;
+  }
+
+  // add new data to beginning of list
+  public void prepend(int data) {
+     Node newNode = new Node(data);
+
+    if (head == null) {
+      tail = newNode;
+    } else {
+      head.prev = newNode;
+      newNode.next = head;
+    }
+    
+    head = newNode;
   }
 
   @Override
   public String toString() {
+    // print every node in the list separated by spaces
     return "To be implemented soon!";
   }
 }
